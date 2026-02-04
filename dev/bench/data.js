51518530e96b8bab67d4b18d443f4e5aaec26148
github-action-benchmark <github@users.noreply.github.com>
2026-02-04 20:21:07 +0000

    add Performance Metrics (customSmallerIsBetter) benchmark result for 1bb6b5495ca49bfbf0ae0bca7927b7088e749bf8

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1770236467728,
+  "repoUrl": "https://github.com/npequeux/rtop",
+  "entries": {
+    "Performance Metrics": [
+      {
+        "commit": {
+          "author": {
+            "email": "npequeux@users.noreply.github.com",
+            "name": "Nicolas Pequeux",
+            "username": "npequeux"
+          },
+          "committer": {
+            "email": "npequeux@users.noreply.github.com",
+            "name": "Nicolas Pequeux",
+            "username": "npequeux"
+          },
+          "distinct": true,
+          "id": "1bb6b5495ca49bfbf0ae0bca7927b7088e749bf8",
+          "message": "fix: Pre-fetch gh-pages branch for benchmark action\n\nAdd step to fetch gh-pages branch before running benchmark action to avoid fetch errors",
+          "timestamp": "2026-02-04T21:20:01+01:00",
+          "tree_id": "677cbbc2156d3a5b16f193c8cf9426be8e9b7050",
+          "url": "https://github.com/npequeux/rtop/commit/1bb6b5495ca49bfbf0ae0bca7927b7088e749bf8"
+        },
+        "date": 1770236467439,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Binary Size (bytes)",
+            "value": 3409752,
+            "unit": "bytes"
+          },
+          {
+            "name": "Build Time",
+            "value": 55.21,
+            "unit": "seconds"
+          }
+        ]
+      }
+    ]
+  }
+}
